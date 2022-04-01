1094d2f4f6fcc185fb7c3244be39402d35680f75
RAFAELESPADA <34714331+RAFAELESPADA@users.noreply.github.com>
2022-04-01 11:20:56 -0300

    Update Soup.java

--- Listeners/Soup.java
+++ Listeners/Soup.java
@@ -33,14 +33,14 @@ import me.RafaelAulerDeMeloAraujo.SpecialAbility.Join;
 /*    */     }
 
 Player p = e.getPlayer();
-/* 33 */       if (p.getHealth() < 20.0D && Join.game.contains(p.getName()) && (p.getItemInHand().getType() == Material.MUSHROOM_SOUP)) {
+/* 33 */       if (p.getHealth() < 20.0D && Join.game.contains(p.getName()) && (p.getItemInHand().getType() == Material.MUSHROOM_STEW)) {
 	e.setCancelled(true);
 /* 34 */         p.playSound(p.getLocation(), Sound.valueOf(this.main.getConfig().getString("Sound.Soup")), 2.0F, 5.0F);
 /* 35 */        
 /* 36 */         
 /* 37 */         p.setHealth(p.getHealth() + this.vida >= 20.0D ? 20.0D : p.getHealth() + this.vida);
 p.setFoodLevel(20);
-/* 38 */          if(main.getConfig().getBoolean(".KeepBowl"))
+/* 38 */         if(main.getConfig().getBoolean(".KeepBowl"))
     e.getItem().setType(Material.BOWL);
 else
     e.getItem().setAmount(0);
